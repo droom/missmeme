15832cf5284480e9003e6c321033a8f6b91a4b41
Chris Droom <github@droom.io>
2022-07-28 20:45:43 +0100

    v1a

--- missmeme.js
+++ missmeme.js
@@ -10,19 +10,23 @@ $(document).ready(function(){
 
 	$('.shelf').slick('slickGoTo', 2);
 
-	TweenLite.to("textarea", 3, {text:"Type here to add your content", ease:Linear.easeNone})
+	TweenLite.to('#statement', 3, {text:'Type here to add your content', ease:Linear.easeNone})
 
 	$('#statement').keyup(function() {
 		var length = $(this).val().length;
 		if (length <= 16){
-			$('#chars').text("one line");
-			$('#statement').attr("rows", "1");
+			$('#chars').text('one line');
+			$('#statement').attr('rows', '1');
 		} else {
-			$('#chars').text("two lines");
-			$('#statement').attr("rows", "2");
+			$('#chars').text('two lines');
+			$('#statement').attr('rows', '2');
 		};
 	});
 
+	$('#download').click(function() {
+		html2canvas (document. querySelector('#capture')) . then (canvas=>{
+			document. body. appendChild (canvas)
+		});
 
-
+	});
 });
