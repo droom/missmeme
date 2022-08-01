10cba5c3ef9c7dac45662316b9542d701ba1cec8
Chris Droom <github@droom.io>
2022-08-01 09:09:09 +0100

    mobile

--- missmeme.js
+++ missmeme.js
@@ -23,10 +23,9 @@ $(document).ready(function(){
 		};
 	});
 
-	$('#download').click(function() {
-		html2canvas (document. querySelector('#capture')) . then (canvas=>{
-			document. body. appendChild (canvas)
-		});
 
-	});
+
+
+
+
 });
